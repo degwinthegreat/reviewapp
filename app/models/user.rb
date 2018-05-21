7a523d9cfe37f830016c7d0ab4cc29d19aa87234
degwinthegreat <the.colledge.dropout@gmail.com>
2018-05-22 06:48:47 +0900

    test

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, omniauth_providers: %i(google facebook)
+         :omniauthable, omniauth_providers: %i(google facebook twitter)
 
   def self.create_unique_string
     SecureRandom.uuid

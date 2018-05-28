c2d43f63c6be6bbaf5dc600d156b73d9fcbf6e6d
degwinthegreat <the.colledge.dropout@gmail.com>
2018-05-29 03:20:58 +0900

    complete test

--- app/models/estimate.rb
+++ app/models/estimate.rb
@@ -1,4 +1,5 @@
 class Estimate < ApplicationRecord
   belongs_to :user
   belongs_to :citicize
+  validates :rate, numericality: { only_integer: true, less_than_or_equal_to: 5, greater_than_or_equal_to: 1}
 end
